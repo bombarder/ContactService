12ec24a715a2320acb55e780ac1303c0d64137ea
nmakarov <nmakarov@playtika.com>
2016-12-26 19:16:45 +0200

    testMassage improved

--- Model/src/test/java/fromJsonTest.java
+++ Model/src/test/java/fromJsonTest.java
@@ -12,12 +12,21 @@ public class fromJsonTest {
         TestCustomer expectedCustomer = new TestCustomer();
         expectedCustomer.setAge(30);
 
-        String byteMessage = "{\n" +
-                "  \"age\": \"30\"\n" +
+        //language=JSON
+        String testMessage = "{\n" +
+                "\"firstName\": \"John\",\n" +
+                "\"hobby\": \"golf\",\n" +
+                "\"birthDate\": \"01-01-2017\", \n" +
+                "\"age\": \"30\",\n" +
+                "\"byteNumber\": \"2\",\n" +
+                "\"shortNumber\": \"4\",\n" +
+                "\"longNumber\": \"220\",\n" +
+                "\"floatNumber\":\"1000f\",\n" +
+                "\"doubleNumber\":\"2000d\"\n" +
                 "}";
 
         JsonParser jsonParser = new JsonParser();
-        TestCustomer testCustomer = jsonParser.fromJson(byteMessage, TestCustomer.class);
+        TestCustomer testCustomer = jsonParser.fromJson(testMessage, TestCustomer.class);
         assertEquals(expectedCustomer ,testCustomer);
     }
 }
